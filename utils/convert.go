c8bfad9dee0b41619f43fb40cb5314ef9bc2a58d
RickyJian <rjricky03bc@gmail.com>
2021-01-14 21:45:12 +0800

    add batch utils

--- utils/convert.go
+++ utils/convert.go
@@ -1,5 +1,9 @@
 package utils
 
+import (
+	"context"
+)
+
 // Converter is the interface that wraps basic Convert method
 type Converter interface {
 	Convert() (map[int]string, error)
@@ -8,4 +12,81 @@ type Converter interface {
 // Convert convert chinese from simplicity to traditional or simplicity to traditional
 func Convert(c Converter) (map[int]string, error) {
 	return c.Convert()
+}
+
+const (
+	// batchWorker defines worker number
+	batchWorker = 50
+	// batchQueue defines queue number
+	batchQueue = 100
+)
+
+// ConvertFunc defines convert func when doing batch
+type ConvertFunc func(in string) (string, error)
+
+type batchResult struct {
+	batch int
+	text  string
+	err   error
+}
+
+// Batch defines bytes batch processing
+func Batch(bytes [][]byte, processingFunc ConvertFunc) (map[int]string, error) {
+	byteLen := len(bytes)
+	if byteLen == 0 {
+		return map[int]string{}, nil
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	worker := make(chan struct{}, batchWorker)
+	queue := make(chan *batchResult, batchQueue)
+
+	done := make(chan interface{})
+	results := make(map[int]string, byteLen)
+	byteLen--
+	var err error
+	go func() {
+		var count int
+	LOOP:
+		for q := range queue {
+			results[q.batch] = q.text
+			if err != nil || count == byteLen {
+				err = q.err
+				cancel()
+				close(done)
+				break LOOP
+			}
+			count++
+		}
+	}()
+
+LOOP:
+	for i, bs := range bytes {
+		worker <- struct{}{}
+		select {
+		case <-ctx.Done():
+			break LOOP
+		default:
+			go func(batch int, bs []byte) {
+				select {
+				case <-ctx.Done():
+					// if error occur do nothing
+				default:
+					queue <- func(ctx context.Context, batch int, bs []byte, process ConvertFunc) *batchResult {
+						text, err := process(string(bs))
+						return &batchResult{
+							batch: batch,
+							text:  text,
+							err:   err,
+						}
+					}(ctx, batch, bs, processingFunc)
+				}
+				<-worker
+			}(i, bs)
+		}
+	}
+	<-done
+	return results, err
 }
--- utils/convert_test.go
+++ utils/convert_test.go
@@ -30,3 +30,27 @@ func TestConvert(t *testing.T) {
 		assert.Equal(t, test.expectedErr, err)
 	}
 }
+
+
+func TestBatch(t *testing.T) {
+	mockConvertFunc := func(in string) (string, error) {
+		return in, nil
+	}
+	var tests = []*struct {
+		src  string
+	}{
+		{
+			src: "../test/thousand_character_classic_cht",
+		},
+		{
+			src: "../test/three_character_classic_cht",
+		},
+	}
+	for _, test := range tests {
+		bs, _ := ReadAll(test.src)
+		results, _ := Batch(bs, mockConvertFunc)
+		for i := 0; i < len(results); i++ {
+			assert.Equal(t, string(bs[i]), results[i])
+		}
+	}
+}
